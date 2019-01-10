0f7bf9b53e123de31ce3ae8ebca7db821f66012a
Brad Slavens <Bradslavens@users.noreply.github.com>
2019-01-09 18:01:45 -0800

    app.js returns html after searching array based on question

--- app.js
+++ app.js
@@ -5,12 +5,24 @@ app.use(express.static(__dirname))
 
 port = process.env.PORT
 
+var questions = [
+    {
+        answer:"I want to buy something", 
+        response:"<h4 class='question>What would you like to buy?</h4>" 
+    },
+    {
+        answer:"Hat",
+        response:"<h4>Great! How many would you like?</h4><input type='text'><button>Submit</button>"
+    }
+    ];
+
 app.get("/", (req, res)=>{
     res.sendFile(__dirname + "/index.html")
 })
 
 app.get("/answer/:answer", (req,res)=>{
-    console.log(req.params);
+    var item = questions.find(item => item.answer === req.params.answer);
+    res.send(item);
 })
 
 app.listen(port, ()=>{
--- myscripts.js
+++ myscripts.js
@@ -2,8 +2,16 @@
 $( document).ready( function(){
     $(".answer").on("click", function(){
         var answer = $(this).attr("name");
-        $.get("/answer/" + answer, function( data ){
-            console.log(data.htmlString);
+        $.get("/answer/" + answer).done( function( data ){
+            // data.response is what  we want
+            htmlString = '<div class="row">';
+            htmlString += '<div class="one-half column" style="margin-top: 5%">';
+            htmlString += data.response;
+            htmlString += '</div>';
+            htmlString += '</div>';
+
+
+            $(".container").append(htmlString);
         });
         // var answer = $(this).attr("name");
         // console.log(htmlString);
